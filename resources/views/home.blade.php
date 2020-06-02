fb8d9221c4354006669d485688ddb3a8f8b34033
Renan Oliveira <renan_oliveira117@hotmail.com>
2020-06-02 18:11:46 +0000

    commit inicial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Sistema educacional')
+
+@section('content_header')
+    <h1>adminlte</h1>
+@stop
+
+@section('content')
+    <p>Awe adminlte.</p>
+@stop
+
+@section('css')
+    
+@stop
+
+@section('js')
+    
+@stop
